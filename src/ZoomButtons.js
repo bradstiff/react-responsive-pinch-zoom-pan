27d0cf4659c0f37d312fd12e2354a68ceb2cee4b
hassanhafezzz <hassan1hafez@gmail.com>
2020-04-09 00:38:15 +0200

    use 2nd level import for better tree shaking for fort awesome

--- src/ZoomButtons.js
+++ src/ZoomButtons.js
@@ -1,41 +1,57 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import faPlus from "@fortawesome/free-solid-svg-icons/faPlus";
+import faMinus from "@fortawesome/free-solid-svg-icons/faMinus";
+import "./styles.css";
 
-import './styles.css';
-
-const containerStyle = { 
-    position: 'absolute', 
-    zIndex: 1000 
+const containerStyle = {
+  position: "absolute",
+  zIndex: 1000
 };
 
 const ZoomOutButton = ({ disabled, onClick }) => (
-    <button className='iconButton' style={{ margin: '10px' }} onClick={onClick} disabled={disabled}>
-        <FontAwesomeIcon icon={faMinus} />
-    </button>
+  <button
+    className="iconButton"
+    style={{ margin: "10px" }}
+    onClick={onClick}
+    disabled={disabled}
+  >
+    <FontAwesomeIcon icon={faMinus} />
+  </button>
 );
 
 const ZoomInButton = ({ disabled, onClick }) => (
-    <button className='iconButton' style={{ margin: '10px', marginLeft: '0px' }} onClick={onClick} disabled={disabled}>
-        <FontAwesomeIcon icon={faPlus} />
-    </button>
+  <button
+    className="iconButton"
+    style={{ margin: "10px", marginLeft: "0px" }}
+    onClick={onClick}
+    disabled={disabled}
+  >
+    <FontAwesomeIcon icon={faPlus} />
+  </button>
 );
 
-const ZoomButtons = ({scale, minScale, maxScale, onZoomInClick, onZoomOutClick}) => (
-    <div style={containerStyle}>
-        <ZoomOutButton onClick={onZoomOutClick} disabled={scale <= minScale} />
-        <ZoomInButton onClick={onZoomInClick} disabled={scale >= maxScale} />
-    </div>
+const ZoomButtons = ({
+  scale,
+  minScale,
+  maxScale,
+  onZoomInClick,
+  onZoomOutClick
+}) => (
+  <div style={containerStyle}>
+    <ZoomOutButton onClick={onZoomOutClick} disabled={scale <= minScale} />
+    <ZoomInButton onClick={onZoomInClick} disabled={scale >= maxScale} />
+  </div>
 );
 
 ZoomButtons.propTypes = {
-    scale: PropTypes.number.isRequired,
-    minScale: PropTypes.number.isRequired,
-    maxScale: PropTypes.number.isRequired,
-    onZoomInClick: PropTypes.func.isRequired,
-    onZoomOutClick: PropTypes.func.isRequired,
+  scale: PropTypes.number.isRequired,
+  minScale: PropTypes.number.isRequired,
+  maxScale: PropTypes.number.isRequired,
+  onZoomInClick: PropTypes.func.isRequired,
+  onZoomOutClick: PropTypes.func.isRequired
 };
 
 export default ZoomButtons;
